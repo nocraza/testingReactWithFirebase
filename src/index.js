dc35e8518995cde0240f9012f13a6e6e4e42f783
nocraza <leiranocraza@gmail.com>
2019-04-25 19:43:13 +0800

    not yet done. still to connect firebase with react and redux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+import {Provider} from 'react-redux'
+
+import store from './store'
+import Firebase, {Firebase Con}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
